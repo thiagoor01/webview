5c2369d42ca3cbde36d602659b47ede7876a27f7
thiagao <thiagobsouza05@gmail.com>
2025-06-25 11:05:24 -0500

    ultimo?

--- js/script.js
+++ js/script.js
@@ -18,27 +18,10 @@ function updateTime() {
 
   document.addEventListener("DOMContentLoaded", function () {
     const carousel = document.getElementById("carousel");
-    const prevBtn = document.getElementById("prevBtn");
-    const nextBtn = document.getElementById("nextBtn");
 
     let currentIndex = 0;
     let autoSlideInterval;
 
-    nextBtn.addEventListener("click", function () {
-        if (currentIndex < carousel.children.length - 1) {
-            currentIndex++;
-            updateCarousel();
-            resetAutoSlideInterval();
-        }
-    });
-
-    prevBtn.addEventListener("click", function () {
-        if (currentIndex > 0) {
-            currentIndex--;
-            updateCarousel();
-            resetAutoSlideInterval();
-        }
-    });
 
     function updateCarousel() {
         const translateValue = -currentIndex * 100 + "%";
@@ -61,12 +44,3 @@ function updateTime() {
     
     resetAutoSlideInterval();
 });
-
-function enviarFormulario(event) {
- 
-    event.preventDefault();
-  
-    alert('Os dados foram enviados com sucesso, obrigado!');
-
-    window.location.href = 'index.html';
-}
